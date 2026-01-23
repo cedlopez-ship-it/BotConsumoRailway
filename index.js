ff89e3cc4ceb02a814ae6eba7611c7c01b023293
cedlopez-ship-it <cedlopez@gmail.com>
2026-01-23 14:16:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
-const express = require("express");
-const axios = require("axios");
-const bodyParser = require("body-parser");
-
 const app = express();
 app.use(bodyParser.json());
 
